1e8886eb1e13fdd690646b8c5d5325cc1b64e918
Modernization Maintainer <modernization@example.invalid>
2026-06-09 15:34:26 -0400

    Harden Playwright Docker install path

--- test/deployment_container_contract_test.js
+++ test/deployment_container_contract_test.js
@@ -7,12 +7,16 @@ var path = require('path');
 var root = path.join(__dirname, '..');
 var appDockerfile = fs.readFileSync(path.join(root, 'Dockerfile'), 'utf8');
 var apiDockerfile = fs.readFileSync(path.join(root, 'Dockerfile.modern-api'), 'utf8');
+var browserTestDockerfile = fs.readFileSync(path.join(root, 'Dockerfile.start-local-browser-test'), 'utf8');
 var compose = fs.readFileSync(path.join(root, 'docker-compose.integrated.yml'), 'utf8');
 var publicCompose = fs.readFileSync(path.join(root, 'docker-compose.public-api.yml'), 'utf8');
 
 assert(appDockerfile.indexOf('ENV NODE_ENV=production') !== -1);
 assert(apiDockerfile.indexOf('scripts/verify_deployment_config.js') !== -1);
 assert(apiDockerfile.indexOf('--service=api') !== -1);
+assert(browserTestDockerfile.indexOf('RUN npm ci') !== -1);
+assert(browserTestDockerfile.indexOf('RUN npx playwright install --with-deps') !== -1);
+assert(browserTestDockerfile.indexOf('playwright@') === -1);
 assert(compose.indexOf('node scripts/verify_deployment_config.js --service=app') !== -1);
 assert(compose.indexOf('node scripts/verify_deployment_config.js --service=api') !== -1);
 assert(publicCompose.indexOf('UTOPLAN_API_BIND') !== -1);
