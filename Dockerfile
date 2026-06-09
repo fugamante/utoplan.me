3362e4b32ae287b06039cac20d4f866960706ac1
Modernization Maintainer <modernization@example.invalid>
2026-06-09 07:31:16 -0400

    Align app container runtime defaults

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:22-bookworm-slim
 
 WORKDIR /workspace
 
-ENV NODE_ENV=development
+ENV NODE_ENV=production
 ENV PORT=8080
 
 COPY .node-version ./
--- test/deployment_container_contract_test.js
+++ test/deployment_container_contract_test.js
@@ -5,10 +5,12 @@ var fs = require('fs');
 var path = require('path');
 
 var root = path.join(__dirname, '..');
+var appDockerfile = fs.readFileSync(path.join(root, 'Dockerfile'), 'utf8');
 var apiDockerfile = fs.readFileSync(path.join(root, 'Dockerfile.modern-api'), 'utf8');
 var compose = fs.readFileSync(path.join(root, 'docker-compose.integrated.yml'), 'utf8');
 var publicCompose = fs.readFileSync(path.join(root, 'docker-compose.public-api.yml'), 'utf8');
 
+assert(appDockerfile.indexOf('ENV NODE_ENV=production') !== -1);
 assert(apiDockerfile.indexOf('scripts/verify_deployment_config.js') !== -1);
 assert(apiDockerfile.indexOf('--service=api') !== -1);
 assert(compose.indexOf('node scripts/verify_deployment_config.js --service=app') !== -1);
